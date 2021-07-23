edf4db0376e8d7fc4bf54fe96ffbb7f91e84ea59
shoyahikichi <yindeshouya@gmail.com>
2021-07-23 23:16:12 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,20 @@ class Item < ApplicationRecord
   belongs_to :preparation_day
   has_one_attached :image
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :price, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  end
+
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :item_condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :preparation_day_id
+end
+
 end
