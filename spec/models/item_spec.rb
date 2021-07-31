7adea81a1e46ebf28507e7f810a44aa406521421
shoyahikichi <yindeshouya@gmail.com>
2021-07-31 11:49:49 +0900

    Rubocopを実行。自動修正。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
       it 'priceが¥300~¥9,999,999の間以外だと出品できない(9,999,999以上)' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
