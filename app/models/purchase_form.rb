7fdb3b7c3de13947c43a137591f5ef5305bf0121
shoyahikichi <yindeshouya@gmail.com>
2021-08-01 20:20:51 +0900

    カラム名訂正。バリデーション。

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,7 +1,18 @@
 class PurchaseForm
   include ActiveModel::Model
 
-  attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name, :purchase
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :phone_number, :building_name, :purchase
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :phone_number
+    validates :purchase
+  end
 
   
+
 end
