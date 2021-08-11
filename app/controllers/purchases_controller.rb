ccd8c67acc8283ba32f1fd044d7dcca38f1d4cdf
shoyahikichi <yindeshouya@gmail.com>
2021-08-11 23:45:06 +0900

    商品購入ページへの制限

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,12 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
     @purchase = PurchaseForm.new
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
