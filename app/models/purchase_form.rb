ca94f794ff5484cb46522aeadb56904719016fc5
shoyahikichi <yindeshouya@gmail.com>
2021-08-11 22:56:26 +0900

    エラー表示

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -5,11 +5,11 @@ class PurchaseForm
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code
-    validates :prefecture_id
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly'}
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{11}\z/, message: 'Input only number'}
   end
 
   def save
