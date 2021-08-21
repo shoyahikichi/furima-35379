ce770f8d86920af5cf833433e97de68cb1d6d1c8
shoyahikichi <yindeshouya@gmail.com>
2021-08-21 17:33:33 +0900

    テスト修正しました。

--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -15,44 +15,63 @@ RSpec.describe PurchaseForm, type: :model do
       it 'ログイン状態の出品者以外のユーザーのみ、必要な情報を適切に入力すると、商品の購入ができること' do
         expect(@purchase_form).to be_valid
       end
+      it '建物名が空でも購入できる' do
+        @purchase_form.building_name = ''
+        expect(@purchase_form).to be_valid
+      end
     end
     context '商品購入できないとき' do
-      it '郵便番号の保存にはハイフンがないと保存できない' do
+      it 'userが紐づいていないと購入できないこと' do
+        @purchase_form.user_id = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("User can't be blank")
+      end
+      it 'itemが紐づいていないと購入できないこと' do
+        @purchase_form.item_id = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Item can't be blank")
+      end
+      it '郵便番号の保存にはハイフンがないと購入できない' do
         @purchase_form.postal_code = '1112222'
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include('Postal code Input correctly')
       end
-      it '郵便番号が空では保存できない' do
+      it '郵便番号が空では購入できない' do
         @purchase_form.postal_code = ''
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("Postal code can't be blank")
       end
-      it '都道府県が---だと保存できない' do
+      it '都道府県が---だと購入できない' do
         @purchase_form.prefecture_id = 1
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include('Prefecture Select')
       end
-      it '市区町村が空では保存できない' do
+      it '市区町村が空では購入できない' do
         @purchase_form.city = ''
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("City can't be blank")
       end
-      it '番地が空では保存できない' do
+      it '番地が空では購入できない' do
         @purchase_form.address = ''
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("Address can't be blank")
       end
-      it '電話番号は11桁以内の数値でないと保存できない' do
+      it '電話番号は11桁以内の数値でないと購入できない' do
         @purchase_form.phone_number = '0901111222233'
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include('Phone number Input only number')
       end
-      it '電話番号が空では保存できない' do
+      it '電話番号が空では購入できない' do
         @purchase_form.phone_number = ''
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("Phone number can't be blank")
       end
-      it 'tokenが空では保存ができない' do
+      it '電話番号は英数字混合では購入できない' do
+        @purchase_form.phone_number = '090asdf12e3'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include('Phone number Input only number')
+      end
+      it 'tokenが空では購入ができない' do
         @purchase_form.token = nil
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("Token can't be blank")
